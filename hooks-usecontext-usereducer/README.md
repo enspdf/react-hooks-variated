4b605168f33ef794b319571b8824d375cd5d1d60
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-01-21 05:41:05 -0500

    Hooks useContext && useReducer

---
+++ hooks-usecontext-usereducer/README.md
@@ -0,0 +1,2 @@
+# Tutorial
+[Source Tutorial](https://www.youtube.com/watch?v=cjBm0HnYcqw)
---
+++ hooks-usecontext-usereducer/src/components/NewestPerson.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+const NewestPerson = ({ newestPerson }) => {
+    useEffect(() => {
+        const newestPersonName = `${newestPerson.firstName} ${newestPerson.lastName}`;
+
+        document.title = newestPersonName;
+
+        console.log("useEffect");
+
+        return () => {
+            console.log("Unmounted");
+        };
+    }, [newestPerson]);
+
+    return (
+        <div className="col col-sm-12">
+            <h2 className="mt-4 text-center">
+                Newes Person: {`${newestPerson.firstName} ${newestPerson.lastName}`}
+            </h2>
+            {/*<PeopleCount />*/}
+        </div>
+    );
+};
+
+export default NewestPerson;
