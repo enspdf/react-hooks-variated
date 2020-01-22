112254a1d80fd5344aa6ba591373552905b4f05e
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-01-21 23:49:51 -0500

    custom-hook-sample-form

---
+++ custom-hook-sample-form/src/App.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import useForm from "./useForm";
+
+const initialValues = {
+  email: "",
+  password: "",
+  isOk: false,
+  gender: "female",
+  car: "ford"
+};
+
+function App() {
+  const form = useForm({ initialValues });
+
+  useEffect(() => {
+    if (form.fields.gender === "male") {
+      form.addField("extra", "");
+    } else {
+      form.removeField("extra");
+    }
+  }, [form.fields.gender]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form.fields);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <span>Email</span>
+        <br />
+        <input type="email" {...form.getInput("email")} />
+        <br />
+        <span>Password</span>
+        <br />
+        <input type="password" {...form.getInput("password")} />
+        <br />
+        <input type="checkbox" {...form.getCheckbox("isOk")} />
+        <span>Is Ok?</span>
+        <br />
+        <input type="radio" {...form.getRadio("gender", "male")} />
+        <span>Male</span>
+        <br />
+        <input type="radio" {...form.getRadio("gender", "female")} />
+        <span>Female</span>
+        <br />
+        <select {...form.getSelect("car")}>
+          <option value="audi">audi</option>
+          <option value="ford">ford</option>
+          <option value="mercedes">mercedes</option>
+        </select>
+        <br />
+        {form.fields.gender === "male" && (
+          <input type="text" {...form.getInput("extra")} />
+        )}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
