72ff415b3407ae191cb13473351d181632cb5a89
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-02-12 23:33:07 -0500

    Finish swr use

--- use-swr-hook-crime-mapping/src/App.jsx
+++ use-swr-hook-crime-mapping/src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import useSWR, { SWRConfig } from "swr";
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function App() {
   return (
-    <SWRConfig value={{ fetcher }}>
+    <SWRConfig value={{ revalidateOnFocus: false, fetcher }}>
       <Crimes />
     </SWRConfig>
   );
 };
 
 function Crimes() {
-  const url = "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";:
+  const url = "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
   const { data, error } = useSWR(url);
 
   if (error) {
@@ -23,5 +23,19 @@ function Crimes() {
     return <div>Loading...</div>
   }
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
-}
+  return <DisplayCrimes crimes={data} categories={[... new Set(data.map(crime => crime.category))]} />
+};
+
+function DisplayCrimes({ crimes, categories }) {
+  const [filterCategory, setFilterCategory] = useState(null);
+
+  const filteredCrimes = filterCategory ? crimes.filter(crime => crime.category === filterCategory) : crimes;
+
+  return (
+    <>
+      {categories.map(category => <button onClick={() => setFilterCategory(category)} key={category}>{category}</button>)}
+      {filterCategory && <button onClick={() => { setFilterCategory(null) }}>reset</button>}
+      <pre>{JSON.stringify(filteredCrimes, null, 2)}</pre>
+    </>
+  );
+};
